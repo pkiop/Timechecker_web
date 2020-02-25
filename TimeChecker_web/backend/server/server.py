1e885ce4002dd753b108a0b462ddf5e76caa9a1c
Park Sang Shin <34783156+pkiop@users.noreply.github.com>
2020-02-25 23:19:16 +0900

    .

--- TimeChecker_web/backend/server/server.py
+++ TimeChecker_web/backend/server/server.py
@@ -5,20 +5,24 @@ main server
 
 from flask import Flask, render_template, request
 import os
+import pymysql
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return "Hello World!"
+def main():
+	return render_template('index.html')
+
+@app.route('/input')
+def input():
+    return render_template('input.html')
 
 @app.route('/abc')
 def abc():
 	return "ABC"
 
-@app.route('/login2', methods=['POST'])
-def login2():
+@app.route('/to_engine', methods=['POST'])
+def to_engine():
 	print("here OK")
 	#v = request.form['main2']
 	day = request.form.get('day')
@@ -31,8 +35,44 @@ def login2():
 	f.write(v)
 	f.close()
 	os.system("real_web_time_db.exe")
+	os.system("timechecker_processing.exe " + day)
 	if request.method == 'POST':
-		return 'POST'
+		return 'POST, to_engine'
+
+@app.route('/show_data')
+def show_data():
+	print("in show_data")
+	return render_template('show_data.html')
+
+@app.route('/get_data', methods=['POST'])
+def get_data():
+	print("get_DATA")
+	html = "<h1>get_DATA</h1>";
+	conn = pymysql.connect(host = '127.0.0.1', user = 'root', password = 'q1w2e3r4!!', db = 'test')
+	curs = conn.cursor()
+
+	day = request.form.get('day')
+	print(day)
+
+	sql = "select * from a" + day
+	try:
+		curs_ret = curs.execute(sql)
+		print(curs_ret)
+
+		rows = curs.fetchall()
+
+		for i in rows:
+			for j in i:
+				html += str(j) + '\t'
+			html += '<br>'
+
+		conn.close()
+		return html
+	except:
+		return day + "는 없습니다. "
+	
+	
+
 
 if __name__ == '__main__':
     app.run()
